4b85a3b4fb9bbb4d72df6d6b72051e02111542b8
Rafael Santos <rafaelnsantos1@gmail.com>
2023-08-23 20:13:53 -0300

    refactor

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,4 +2,33 @@
 
 import dynamic from "next/dynamic"
 
-export default dynamic(() => import('../../components/Admin'), { ssr: false, loading: () => <div>Carregando...</div> })
+const Admin = dynamic(() => import('../../components/Admin'), { ssr: false, loading: () => <div>Carregando...</div> })
+
+export default function AdminPage() {
+    return <>
+      <Admin 
+        cms={{
+          backend: {
+            name: 'git-gateway',
+            branch: 'master',
+            gateway_url: `https://staticcms.netlify.app/.netlify/git/github`
+        },
+          media_folder: 'public/uploads',
+          public_folder: '/uploads',
+          collections: [
+            {
+              name: 'pages',
+              label: 'Pages',
+              folder: 'src/pages',
+              fields: [
+                { name: 'title', label: 'Title', widget: 'string' },
+                { name: 'path', label: 'Path', widget: 'string' },
+                { name: 'body', label: 'Body', widget: 'markdown' }
+                ]
+            }
+          ]
+        }}
+        identity={{}}
+      />
+    </>
+}
--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -2,38 +2,21 @@
 
 import { useEffect } from 'react'
 import '@staticcms/core/dist/main.css';
-import CMS from '@staticcms/core';
+import CMS, { Config } from '@staticcms/core';
 import identity from 'netlify-identity-widget';
 
-export default function Admin() {
+interface AdminProps {
+    cms?: Config
+    identity?: identity.InitOptions
+}
+
+export default function Admin(props: AdminProps) {
     useEffect(() => {
-        CMS.init({
-            config: {
-                backend: {
-                    name: 'git-gateway',
-                    branch: 'master',
-                    gateway_url: `https://staticcms.netlify.app/.netlify/git/github`
-                },
-                media_folder: 'public/uploads',
-                public_folder: '/uploads',
-                collections: [
-                    {
-                        name: 'pages',
-                        label: 'Pages',
-                        folder: 'src/pages',
-                        fields: [
-                            { name: 'title', label: 'Title', widget: 'string' },
-                            { name: 'path', label: 'Path', widget: 'string' },
-                            { name: 'body', label: 'Body', widget: 'markdown' }
-                        ]
-                    }
-                ]
-            }
-        });
+        CMS.init({ config: props.cms });
 
         (window as any).netlifyIdentity = identity;
-        identity.init({});
 
+        identity.init(props.identity);
     }, [])
 
     return <></>
