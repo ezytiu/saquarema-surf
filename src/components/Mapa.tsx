f1ef80a29814d31c1912e6e2f7523ae92785aa26
Rafael Santos <rafaelnsantos1@gmail.com>
2023-08-24 00:17:36 -0300

    location added

--- src/components/Mapa.tsx
+++ src/components/Mapa.tsx
@@ -1,16 +1,42 @@
 "use client"
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect, useRef, useState } from 'react';
+
+const error: PositionErrorCallback = (error) => {
+  console.log("Unable to retrieve your location", error);
+}
 
 export default function Mapa() {
+  const [latitude, setLatitude] = useState(-22.91);
+  const [longitude, setLongitude] = useState(-42.49);
+  const mapRef = useRef<Map>(null)
+
+  useEffect(() => {
+    const success: PositionCallback = (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      mapRef.current?.flyTo([latitude, longitude], 14)
+      setLatitude(latitude);
+      setLongitude(longitude);
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.log("Geolocation not supported");
+    }
+  }, [])
+
   return (
-    <MapContainer center={[-22.91, -42.49]} zoom={14} scrollWheelZoom={true}>
+    <MapContainer center={[latitude, longitude]} zoom={14} scrollWheelZoom={true} ref={mapRef}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[-22.9369, -42.4915]}>
+        <Marker position={[latitude, longitude]}>
             <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
